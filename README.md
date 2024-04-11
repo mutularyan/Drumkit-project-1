1b66de6c9e814a18ae8275046d2c907126f23482
ryanmutula <ryan.mutula@student.moringaschool.com>
2024-04-11 17:57:51 +0300

    Project 1 -DrumKitSet A simple drumkit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Drumkit-project-1
+
+## DrumKitSEt
+Crash cymbals button and audio attached
+Kick drum button and audio attached
+Snare drum button and audio attached
+Tom drum button and audio attached
+
+### By Mutula
+
+### Note
+I have used external links from cdnjs for the drum icon used in the heading.
+I have used downloaded images and mp3 files for the audio and background photos of my drums.
+My javascript is well commented on to ensure a smooth understanding for anyone reading my code.
--- index.js
+++ index.js
@@ -1,19 +1,40 @@
+// Array Declaration
+// This creates an array of the four different drum sounds we are going to explore
 const kits = ["crash", "kick", "snare", "tom"];
 
+// DOM Selection
+// This is to find an HTML element with the class name container.(where we will put our buttons)
 const containerEl = document.querySelector(".container");
 
+// This starts a loop, which iterates over each drum sound type, through each element of the 'kits' array.
 kits.forEach((kit) => {
+
+//With each iteration of this loop, a new button element is created and its text is set to the current kit
   const btnEl = document.createElement("button");
   btnEl.classList.add("btn");
   btnEl.innerText = kit;
+
+//Sets background of the button depending on the button sound type
   btnEl.style.backgroundImage = "url(images/" + kit + ".png)";
+
+// This line puts the button inside our container element
   containerEl.appendChild(btnEl);
+
+// With each iteration of this loop, anew audio element is created and the src attribute is set to the relative sound file
   const audioEl = document.createElement("audio");
   audioEl.src = "sounds/" + kit + ".mp3";
+
+// This line puts the audio element inside our container element
   containerEl.appendChild(audioEl);
+
+// This is to add a click event listener to each button so that it plays the corresponding audio 
   btnEl.addEventListener("click", () => {
     audioEl.play();
   });
+
+//   Adding a keyboard event listener
+// When a key is pressed, it checks to see if the key corresponds to the first letter of any of the four drum sound types
+//When the key is clicked, a scaling effect is observed throughout the whole button
   window.addEventListener("keydown", (event) => {
     if (event.key === kit.slice(0, 1)) {
       audioEl.play();
